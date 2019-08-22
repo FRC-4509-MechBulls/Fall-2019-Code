963a184af281d28706bcfd84b5f4c3f6b05787bc
Natasha Setiadi <natset456@gmail.com>
2019-08-22 10:13:06 -0400

    updated autonomous to be more accurate

--- GRITS-2019/src/main/java/frc/robot/commands/AutoCommands.java
+++ GRITS-2019/src/main/java/frc/robot/commands/AutoCommands.java
@@ -21,11 +21,11 @@ public class AutoCommands extends CommandGroup {
 
         // number of seconds in auto mode
         // Use requires() here to declare subsystem dependencies
-        addSequential(new DriveAutonomousCommand(2.3,-0.75,0));
-        addSequential(new DriveAutonomousCommand(0.20,0,-.45));
-        addSequential(new DriveAutonomousCommand(1.30,-0.75,0));
-        addSequential(new DriveAutonomousCommand(.45,0,0.75));
-        addSequential(new DriveAutonomousCommand(.3,-0.55,0));
+        addSequential(new DriveAutonomousCommand(2.3,-0.75,0)); // Goes forward for 2.3 sec
+        addSequential(new DriveAutonomousCommand(0.20,0,-.45)); // Rotates to fix the drift 
+        addSequential(new DriveAutonomousCommand(1.15,-0.75,0)); // Goes forward fpr 1.3 sec
+        addSequential(new DriveAutonomousCommand(.45,0,0.75)); // Rotation 90 degrees clockwise
+        addSequential(new DriveAutonomousCommand(.3,-0.55,0)); 
         
         // addSequential(new DriveAutonomousCommand(.25,0,0.5));
 
